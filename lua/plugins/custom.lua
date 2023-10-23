0b437313158d1dfc33ed1bf24acc8801982006a8
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2023-10-23 21:57:21 +0200

    first commit

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "jsonnet",
+        "proto",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        jsonnet_ls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                fieldalignment = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
+}
